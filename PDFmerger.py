37ac68a60922cd66e824594e79bbaa49bcf176e2
Niels <niels.joseph@physik.tu-darmstadt.de>
2022-12-01 13:40:37 +0100

    Added the ability to change the name of the merged PDF instead of always calling it mergedPDF.

--- PDFmerger.py
+++ PDFmerger.py
@@ -9,14 +9,17 @@ from PyPDF2 import PdfFileMerger
 
 # Set the directory in which the PDFs should be merged
 directory = r"set your own directory"
+merge_name = r"set your own name for the merged file"
 
 
-def merge_pdfs(directory: str) -> None:
+def merge_pdfs(directory: str, merge_name: str) -> None:
     """
-    Merges all the pdfs in a given directory into a new file called mergedPDF that is saved within the same directory.
+    Merges all the pdfs in a given directory into a new file with the name given by merge_name that is saved within the
+    same directory.
     They are merged in alphanumeric order. So numbering them 01, 02, 03... can produce the desired ordering.
     The script cannot merge encrypted PDFs.
     :param directory: The directory of the PDFs that are to be merged.
+    :param merge_name: The name of the merged PDF
     :return:
     """
     # Call the constructor for the file merger
@@ -28,10 +31,10 @@ def merge_pdfs(directory: str) -> None:
             merger.append(os.path.join(directory, file))
 
     # Save the merged files
-    with open(directory + r"\\mergedPDF.pdf", "wb") as f:
+    with open(directory + r"\\" + merge_name + r".pdf", "wb") as f:
         merger.write(f)
 
 
 # proceed only when called as main
 if __name__ == "__main__":
-    merge_pdfs(directory=directory)
+    merge_pdfs(directory=directory, merge_name=merge_name)
